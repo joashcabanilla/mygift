54d1cd5358b57f48faee25fb8c234ddefbfffb93
joashcabanilla <joash0199@gmail.com>
2021-02-13 21:42:54 +0800

    background animation completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import './App.css';
+
 function App(){
     return(
-        <div className="class1">
+        <div className="main-div">
+            <div className="picture">
+                <img className="pic1" src="seventeen_logo.png"/>
+                <img className="pic2" src="seventeen_logo.png"/>
+                <img className="pic3" src="seventeen_logo.png"/>
+                <img className="pic4" src="seventeen_logo.png"/>
+                <img className="pic5" src="seventeen_logo.png"/>
+                <img className="pic6" src="seventeen_logo.png"/>
+            </div>
         </div>
     );
-}
+}   
 export default App;
