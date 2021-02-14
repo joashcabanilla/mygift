cb63254d22de8c49b7eaac9d3bb1016eebe301cd
joashcabanilla <joash0199@gmail.com>
2021-02-14 23:28:09 +0800

    update feb 14

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import {useSpring, animated, config} from 'react-spring';
 import './App.css';
-
 function App(){
+
+    const member1_anim = useSpring(
+        {
+            from: {
+                transform: 'translate3d(0,40px,0)',
+                radians: 0,
+            },
+            to: {
+                transform: 'translate3d(0,0,0)',
+                radians: 5,
+            },
+            config: config.gentle,
+            reset: true,
+        }
+    );
+
     return(
         <div className="main-div">
+            <animated.div className="member1" style={member1_anim}>
+            </animated.div>
             <div className="picture">
                 <img className="pic1" src="seventeen_logo.png"/>
                 <img className="pic2" src="seventeen_logo.png"/>
